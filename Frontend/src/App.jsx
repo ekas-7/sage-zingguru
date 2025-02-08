8c71ff9b4870a2dd2b977fca718768942b711b81
Gaurav Upadhyay <gauravupadhyay.work@gmail.com>
2025-02-08 19:01:08 +0530

    merge fix

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,30 +4,20 @@ import { Provider } from 'react-redux';
 import { store } from './store/store';
 import './index.css';
 
-import LoginButton from "./components/LoginButton.jsx";
 import { useAuth0 } from "@auth0/auth0-react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-import MainStudio from './components/studio/MainStudio.jsx';
 
 const App = () => {
   const { isAuthenticated } = useAuth0();
-  console.log("iss" + isAuthenticated);
 
   return (
     <Router>
-      <div>
-        
-          <Provider store={store}>
-            <Routes>
-              <Route path="/" element={<Landing />} />
-              <Route 
-                path="/dashboard" 
-                element={<Dashboard/>} 
-              />
-            </Routes>
-          </Provider>
-       
-      </div>
+      <Provider store={store}>
+        <Routes>
+          <Route path="/" element={isAuthenticated ? <Navigate to="/dashboard" /> : <Landing />} />
+          <Route path="/dashboard" element={isAuthenticated ? <Dashboard /> : <Navigate to="/" />} />
+        </Routes>
+      </Provider>
     </Router>
   );
 };
