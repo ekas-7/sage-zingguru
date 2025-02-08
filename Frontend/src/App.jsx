6c35880fbb5a6413160d8bb27f97fdccf55d498e
Gaurav Upadhyay <gauravupadhyay.work@gmail.com>
2025-02-08 14:46:52 +0530

    merge fix

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,23 +1,33 @@
-import { Routes,Route } from 'react-router-dom'
 import Dashboard from './Pages/Dashboard'
 import Landing from './Pages/Landing'
 import { Provider } from 'react-redux'
 import { store } from './store/store'
-import Login from './Pages/Login'
 
+import LoginButton from "./components/LoginButton.jsx";
+import { useAuth0 } from "@auth0/auth0-react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-function App() {
+const App = () => {
+  const { isAuthenticated } = useAuth0();
+  console.log(isAuthenticated);
+
   return (
-    <div>
-      <Provider store={store}>
-        <Routes>
-          <Route path="/" element={<Landing/>}/>
-          <Route path="/dashboard" element={<Dashboard/>} />
-          <Route path="/login" element={<Login/>} />
-        </Routes>
-      </Provider>
-    </div>
-  )
-}
+    <Router>
+      <div>
+        {/* <Navbar /> */}
+        {isAuthenticated ? (
+          <Provider store={store}>
+          <Routes>
+            <Route path="/" element={<Landing/>}/>
+            <Route path="/dashboard" element={<Dashboard/>} />
+          </Routes>
+        </Provider>
+        ) : (
+          <LoginButton />
+        )}
+      </div>
+    </Router>
+  );
+};
 
 export default App;
