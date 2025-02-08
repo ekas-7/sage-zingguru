307d7ea2ab296ea1dbac959f9d993b5417cd5fff
Gaurav Upadhyay <gauravupadhyay.work@gmail.com>
2025-02-08 19:03:27 +0530

    fixed auth

--- Frontend/src/components/Dashboard/UserProfile.jsx
+++ Frontend/src/components/Dashboard/UserProfile.jsx
@@ -1,15 +1,23 @@
 import { Avatar, AvatarFallback, AvatarImage } from "./Avatar"
-import placeholderAvatar from '../../assets/placeholderAvatar.png'
+import { useAuth0 } from "@auth0/auth0-react"
+import LogoutButton from "../LogoutButton"
 
-export const UserProfile = () => (
-  <div className="flex items-center space-x-2">
-    <Avatar className="w-8 h-8">
-      <AvatarImage src={placeholderAvatar} />
-      <AvatarFallback>ET</AvatarFallback>
-    </Avatar>
-    <div className="text-sm">
-      <div>Sujal Kumar</div>
-      <div className="text-xs text-gray-400">sujal@gmail.com</div>
+export const UserProfile = () => {
+  const { user, isAuthenticated } = useAuth0()
+
+  if (!isAuthenticated || !user) return null
+
+  return (
+    <div className="flex items-center space-x-2">
+      <Avatar className="w-8 h-8">
+        <AvatarImage src={user.picture || "/default-avatar.png"} />
+        <AvatarFallback>{user.name?.slice(0, 2).toUpperCase()}</AvatarFallback>
+      </Avatar>
+      <div className="text-sm">
+        <div>{user.name}</div>
+        <div className="text-xs text-gray-400">{user.email}</div>
+      </div>
+      <div><LogoutButton/></div>
     </div>
-  </div>
-)
+  )
+}
